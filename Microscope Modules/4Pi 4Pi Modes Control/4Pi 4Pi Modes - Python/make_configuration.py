80f9cee97546af4291e09c69e16b4211c4361b67
Jacopo Antonello <jacopo@antonello.org>
2019-07-23 17:55:22 +0100

    Output configuration into a JSON file

--- Microscope Modules/4Pi 4Pi Modes Control/4Pi 4Pi Modes - Python/make_configuration.py
+++ Microscope Modules/4Pi 4Pi Modes Control/4Pi 4Pi Modes - Python/make_configuration.py
@@ -6,11 +6,14 @@ import numpy as np
 import json  # noqa
 import argparse
 
+from pprint import pprint
 from os import path
 from h5py import File
 from numpy.linalg import matrix_rank
 
 from czernike import RZern
+
+import matplotlib.pyplot as plt  # noqa
 
 
 def get_noll_indices(args):
@@ -42,8 +45,34 @@ def get_noll_indices(args):
         zernike_indices.append(k)
     assert(len(zernike_indices) == zernike_indices1.size)
     zernike_indices = np.array(zernike_indices, dtype=np.int)
+    assert(np.unique(zernike_indices).size == zernike_indices.size)
 
     return zernike_indices
+
+
+def default_name(i, n, m):
+    if i == 1:
+        return 'piston'
+    elif i == 2:
+        return 'tip'
+    elif i == 3:
+        return 'tilt'
+    elif i == 4:
+        return 'defocus'
+    elif m == 0:
+        return 'spherical'
+    elif abs(m) == 1:
+        return 'coma'
+    elif abs(m) == 2:
+        return 'astigmatism'
+    elif abs(m) == 3:
+        return 'trefoil'
+    elif abs(m) == 4:
+        return 'quadrafoil'
+    elif abs(m) == 5:
+        return 'pentafoil'
+    else:
+        return ''
 
 
 if __name__ == '__main__':
@@ -56,12 +85,15 @@ if __name__ == '__main__':
     parser.add_argument(
         '--dm1-calibration', type=str, metavar='HDF5',
         help='Calibration file for DM0 (the second DM powered up in sequence)')
+    parser.add_argument('--flipx', dest='flipx', action='store_true')
+    parser.add_argument('--no-flipx', dest='flipx', action='store_false')
+    parser.set_defaults(flipx=1)
+    parser.add_argument('--flipy', dest='flipy', action='store_true')
+    parser.add_argument('--no-flipy', dest='flipy', action='store_false')
+    parser.set_defaults(flipy=1)
     parser.add_argument(
-        '--flipx', action='store_true', help='Set to 1 to flip along x')
-    parser.add_argument(
-        '--flipy', action='store_true', help='Set to 1 to flip along y')
-    parser.add_argument(
-        '--rotate', type=float, help='Relative pupil rotation in degrees')
+        '--rotate', default=0.0, type=float,
+        help='Relative pupil rotation in degrees')
     parser.add_argument(
         '--disable-dm0', action='store_true',
         help='Disable DM0 (single DM control)')
@@ -142,10 +174,10 @@ NB: DO NOT USE SPACES in this list!''')
         u0 = -np.dot(C0, z0)
         z1[:4] = 0
         u1 = -np.dot(C1, z1)
-        conf['Flats'] = np.concatenate((u0, u1)).tostring()
+        conf['Flats'] = np.concatenate((u0, u1)).tolist()
 
         O1 = np.dot(Fy, np.dot(Fx, R))
-        C0 = np.dot(O1.T, C0)
+        C0 = np.dot(C0, O1.T)
         Nz = H0.shape[0]
         assert(Nz == H1.shape[0])
         zernike_indices = np.arange(1, Nz + 1)
@@ -159,7 +191,45 @@ NB: DO NOT USE SPACES in this list!''')
                 T1[Nz + i, count] = s
                 count += 1
         assert(matrix_rank(T1) == 2*Nz)
+        all_4pi = np.array(all_4pi, dtype=np.int)
 
         zernike_indices = get_noll_indices(args)
         print('Selected Zernike indices are:')
         print(zernike_indices)
+        print()
+
+        inds = []
+        for i in zernike_indices:
+            inds.append(np.where(all_4pi == i)[0][0])
+        check = all_4pi[inds]
+        assert(np.allclose(check, zernike_indices))
+
+        CC = np.zeros((2*C0.shape[0], C0.shape[1] + C1.shape[1]))
+        CC[:C0.shape[0], :C0.shape[1]] = C0
+        CC[C0.shape[0]:, C0.shape[1]:] = C1
+        T3 = np.dot(CC, T1)
+        T4 = T3[:, inds]
+        conf['Matrix'] = T4.tolist()
+
+        ntab = r.ntab
+        mtab = r.mtab
+        modes = []
+        for i in zernike_indices:
+            if i > 0:
+                p = 'co-'
+            else:
+                p = 'contra-'
+            n = default_name(i, ntab[abs(i) - 1], mtab[abs(i) - 1])
+            modes.append(p + n)
+
+        conf['Modes'] = modes
+        print('Selected mode names are:')
+        pprint(modes)
+        print()
+
+        fname = path.join(
+            '..', '4Pi 4Pi Modes - Default Files', 'config.json')
+        with open(fname, 'w') as f:
+            json.dump(conf, f, sort_keys=True, indent=4)
+        print(f'Configuration written to {path.abspath(fname)}')
+        print()
