70afea65c58348b17e50f983b348ad5a52d96250
Jacopo Antonello <jacopo@antonello.org>
2019-07-23 16:38:19 +0100

    Writing make configuration script

--- Microscope Modules/4Pi 4Pi Modes Control/4Pi 4Pi Modes - Python/make_configuration.py
+++ Microscope Modules/4Pi 4Pi Modes Control/4Pi 4Pi Modes - Python/make_configuration.py
@@ -13,49 +13,34 @@ from numpy.linalg import matrix_rank
 from czernike import RZern
 
 
-def get_zernike_indeces_from_args(args):
-    if args.z_min > 0 and args.z_max > 0:
-        mrange = np.arange(args.z_min, args.z_max + 1)
-    else:
-        mrange = np.array([], dtype=np.int)
+def get_noll_indices(args):
+    if args.disable_dm0 or args.disable_dm1:
+        raise NotImplementedError()
 
-    if args.z_noll_include is not None:
-        minclude = np.fromstring(args.z_noll_include, dtype=np.int, sep=',')
-        minclude = minclude[minclude > 0]
-    else:
-        minclude = np.array([], dtype=np.int)
+    noll_min = np.array(args.noll_min, dtype=np.int)
+    noll_max = np.array(args.noll_max, dtype=np.int)
+    minclude = np.array(args.include_4pi, dtype=np.int)
+    mexclude = np.array(args.exclude_4pi, dtype=np.int)
 
-    if args.z_noll_exclude is not None:
-        mexclude = np.fromstring(args.z_noll_exclude, dtype=np.int, sep=',')
-        mexclude = mexclude[mexclude > 0]
-    else:
-        mexclude = np.array([], dtype=np.int)
-
-    zernike_indices = np.setdiff1d(
+    mrange1 = np.arange(noll_min, noll_max + 1, dtype=np.int)
+    mrange = np.zeros(2*mrange1.size)
+    mrange[0::2] = mrange1
+    mrange[1::2] = -mrange1
+    zernike_indices1 = np.setdiff1d(
         np.union1d(np.unique(mrange), np.unique(minclude)),
         np.unique(mexclude))
+    zernike_indices = []
+    for k in minclude:
+        if k in zernike_indices1 and k not in zernike_indices:
+            zernike_indices.append(k)
+    remaining = np.setdiff1d(zernike_indices1, np.unique(zernike_indices))
+    remaining = remaining[np.abs(remaining).argsort()]
+    for k in remaining:
+        zernike_indices.append(k)
+    assert(len(zernike_indices) == zernike_indices1.size)
+    zernike_indices = np.array(zernike_indices, dtype=np.int)
 
-    print('Selected Noll indices for the Zernike modes are:')
-    print(zernike_indices)
-
-    if args.z_4pi_modes and args.z_exclude_4pi_modes:
-        exclude_4pi = np.fromstring(
-            args.z_exclude_4pi_modes, dtype=np.int, sep=',')
-        # intersect = np.intersect1d(zernike_indices, np.abs(exclude_4pi))
-        # if intersect.size != exclude_4pi.size:
-        #     print('WARN: the following 4pi modes exclusions were not used:')
-        #     print(np.setdiff1d(exclude_4pi, intersect))
-    else:
-        exclude_4pi = np.array([], dtype=np.int)
-
-    if args.z_4pi_modes and exclude_4pi.size > 0:
-        print('Excluded 4Pi modes are:')
-        print(exclude_4pi)
-
-    zernike_indices.sort()
-    exclude_4pi.sort()
-
-    return zernike_indices, exclude_4pi
+    return zernike_indices
 
 
 if __name__ == '__main__':
@@ -63,17 +48,11 @@ if __name__ == '__main__':
         description='',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument(
-        '--dm0-index', type=int, default=-1, metavar='INDEX',
-        help='CIUsb index for DM0 (the first DM powered up in sequence)')
+        '--dm0-calibration', type=str, metavar='HDF5',
+        help='Calibration file for DM0 (the first DM powered up in sequence)')
     parser.add_argument(
-        '--dm0-calibration-file', type=str, metavar='HDF5',
-        help='Calibration file for DM0')
-    parser.add_argument(
-        '--dm1-index', type=int, default=-1, metavar='INDEX',
-        help='CIUsb index for DM1 (the first DM powered up in sequence)')
-    parser.add_argument(
-        '--dm1-calibration-file', type=str, metavar='HDF5',
-        help='Calibration file for DM1')
+        '--dm1-calibration', type=str, metavar='HDF5',
+        help='Calibration file for DM0 (the second DM powered up in sequence)')
     parser.add_argument(
         '--flipx', action='store_true', help='Set to 1 to flip along x')
     parser.add_argument(
@@ -87,42 +66,47 @@ if __name__ == '__main__':
         '--disable-dm1', action='store_true',
         help='Disable DM1 (single DM control)')
     parser.add_argument(
-        '--exclude-4pi-modes', type=str, default='-1,2,3,4',
+        '--exclude-4pi', type=str, default='-1,2,3,4',
         metavar='INDICES',
         help='''
 Comma separated list of 4Pi modes to ignore, e.g.,
 -1,2,3,4 to ignore contravariant piston and covariant tip/tilt/defocus.
 The sign denotes co/contra variant. The absolute value denotes a Noll index.
 NB: DO NOT USE SPACES in this list!''')
+    parser.add_argument(
+        '--include-4pi', type=str, default=None, metavar='INDICES',
+        help='''
+Comma separated list of 4Pi modes to include, e.g.,
+-1,2,3,4 to ignore contravariant piston and covariant tip/tilt/defocus.
+The sign denotes co/contra variant. The absolute value denotes a Noll index.
+NB: DO NOT USE SPACES in this list!''')
+    parser.add_argument(
+        '--noll-min', type=int, help='Minimum Zernike Noll index to consider')
+    parser.add_argument(
+        '--noll-max', type=int, help='Minimum Zernike Noll index to consider')
     args = parser.parse_args()
 
-    if args.dm0_index < 0:
-        print('Missing --dm0-index', file=sys.err)
-        sys.exit()
     if (
-            args.dm0_calibration_file is None or
-            not path.isfile(args.dm0_calibration_file)):
+            args.dm0_calibration is None or
+            not path.isfile(args.dm0_calibration)):
         print(
             'Missing or wrong path for --dm0-calibration-file',
-            file=sys.err)
-        sys.exit()
-    if args.dm1_index < 0:
-        print('Missing --dm1-index', file=sys.err)
+            file=sys.stderr)
         sys.exit()
     if (
-            args.dm1_calibration_file is None or
-            not path.isfile(args.dm1_calibration_file)):
+            args.dm1_calibration is None or
+            not path.isfile(args.dm1_calibration)):
         print(
             'Missing or wrong path for --dm1-calibration-file',
-            file=sys.err)
+            file=sys.stderr)
         sys.exit()
 
-    with File(args.dm0_calibration_file, 'r') as f:
+    with File(args.dm0_calibration, 'r') as f:
         H0 = f['/WeightedLSCalib/H'][()]
         C0 = f['/WeightedLSCalib/C'][()]
         z0 = f['/WeightedLSCalib/z0'][()]
         n = f['/WeightedLSCalib/cart/RZern/n'][()]
-    with File(args.dm1_calibration_file, 'r') as f:
+    with File(args.dm1_calibration, 'r') as f:
         H1 = f['/WeightedLSCalib/H'][()]
         C1 = f['/WeightedLSCalib/C'][()]
         z1 = f['/WeightedLSCalib/z0'][()]
@@ -147,6 +131,14 @@ NB: DO NOT USE SPACES in this list!''')
     if args.disable_dm0 or args.disable_dm1:
         raise NotImplementedError()
     else:
+        conf = {}
+
+        z0[:4] = 0
+        u0 = -np.dot(C0, z0)
+        z1[:4] = 0
+        u1 = -np.dot(C1, z1)
+        conf['Flats'] = np.concatenate((u0, u1)).tostring()
+
         O1 = np.dot(Fy, np.dot(Fx, R))
         C0 = np.dot(O1.T, C0)
         Nz = H0.shape[0]
@@ -162,3 +154,7 @@ NB: DO NOT USE SPACES in this list!''')
                 T1[Nz + i, count] = s
                 count += 1
         assert(matrix_rank(T1) == 2*Nz)
+
+        zernike_indices = get_noll_indices(args)
+        print('Selected Zernike indices are:')
+        print(zernike_indices)
