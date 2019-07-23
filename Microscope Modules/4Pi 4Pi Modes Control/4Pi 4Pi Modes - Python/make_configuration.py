725dc0d04277c9b437c522b9e22e1eb04d66dc09
Jacopo Antonello <jacopo@antonello.org>
2019-07-23 16:47:19 +0100

    Select 4Pi Zernike indices

--- Microscope Modules/4Pi 4Pi Modes Control/4Pi 4Pi Modes - Python/make_configuration.py
+++ Microscope Modules/4Pi 4Pi Modes Control/4Pi 4Pi Modes - Python/make_configuration.py
@@ -19,9 +19,12 @@ def get_noll_indices(args):
 
     noll_min = np.array(args.noll_min, dtype=np.int)
     noll_max = np.array(args.noll_max, dtype=np.int)
-    minclude = np.array(args.include_4pi, dtype=np.int)
-    mexclude = np.array(args.exclude_4pi, dtype=np.int)
-
+    minclude = np.array([
+        int(s) for s in args.include_4pi.split(',')
+        if len(s) > 0], dtype=np.int)
+    mexclude = np.array([
+        int(s) for s in args.exclude_4pi.split(',')
+        if len(s) > 0], dtype=np.int)
     mrange1 = np.arange(noll_min, noll_max + 1, dtype=np.int)
     mrange = np.zeros(2*mrange1.size)
     mrange[0::2] = mrange1
@@ -74,16 +77,18 @@ Comma separated list of 4Pi modes to ignore, e.g.,
 The sign denotes co/contra variant. The absolute value denotes a Noll index.
 NB: DO NOT USE SPACES in this list!''')
     parser.add_argument(
-        '--include-4pi', type=str, default=None, metavar='INDICES',
+        '--include-4pi', type=str, default='', metavar='INDICES',
         help='''
 Comma separated list of 4Pi modes to include, e.g.,
 -1,2,3,4 to ignore contravariant piston and covariant tip/tilt/defocus.
 The sign denotes co/contra variant. The absolute value denotes a Noll index.
 NB: DO NOT USE SPACES in this list!''')
     parser.add_argument(
-        '--noll-min', type=int, help='Minimum Zernike Noll index to consider')
+        '--noll-min', type=int, default=5,
+        help='Minimum Zernike Noll index to consider')
     parser.add_argument(
-        '--noll-max', type=int, help='Minimum Zernike Noll index to consider')
+        '--noll-max', type=int, default=22,
+        help='Minimum Zernike Noll index to consider')
     args = parser.parse_args()
 
     if (
@@ -105,7 +110,7 @@ NB: DO NOT USE SPACES in this list!''')
         H0 = f['/WeightedLSCalib/H'][()]
         C0 = f['/WeightedLSCalib/C'][()]
         z0 = f['/WeightedLSCalib/z0'][()]
-        n = f['/WeightedLSCalib/cart/RZern/n'][()]
+        n = int(f['/WeightedLSCalib/cart/RZern/n'][()])
     with File(args.dm1_calibration, 'r') as f:
         H1 = f['/WeightedLSCalib/H'][()]
         C1 = f['/WeightedLSCalib/C'][()]
@@ -144,7 +149,7 @@ NB: DO NOT USE SPACES in this list!''')
         Nz = H0.shape[0]
         assert(Nz == H1.shape[0])
         zernike_indices = np.arange(1, Nz + 1)
-        T1 = np.zeros((Nz, Nz))
+        T1 = np.zeros((2*Nz, 2*Nz))
         all_4pi = list()
         count = 0
         for i, noll in enumerate(zernike_indices):
