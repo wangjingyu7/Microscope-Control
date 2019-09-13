ce47307a3bff1bea352d1d224daff0dcfb4e1290
Jingyu.Wang <wangjingyu7@gmail.com>
2019-09-13 15:57:22 +0100

    This file was previously removed to solve a conflit.

---
+++ Microscope Modules/4Pi 4Pi Modes Control/4Pi 4Pi Modes - Python/run_prompt.bat
@@ -0,0 +1,7 @@
+<<<<<<< HEAD
+@echo off
+Powershell.exe -executionpolicy bypass -NoExit -Command ". .\base.ps1; Activate-Anaconda"
+=======
+@echo off
+Powershell.exe -executionpolicy bypass -NoExit -Command ". .\base.ps1; Activate-Anaconda"
+>>>>>>> origin/ed4pimodes
