d2ac2bb2e9720d98337905b198571bdb34050f20
Jingyu.Wang <wangjingyu7@gmail.com>
2019-09-13 19:54:30 +0100

    All from Jacopo's ed4pimodes.
    Because of the conflicts, I download the module from the repo and copy it to my branch.
    There is nothing from me.

--- Microscope Modules/4Pi 4Pi Modes Control/4Pi 4Pi Modes - Python/run_prompt.bat
+++ Microscope Modules/4Pi 4Pi Modes Control/4Pi 4Pi Modes - Python/run_prompt.bat
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 @echo off
 Powershell.exe -executionpolicy bypass -NoExit -Command ". .\base.ps1; Activate-Anaconda"
-=======
-@echo off
-Powershell.exe -executionpolicy bypass -NoExit -Command ". .\base.ps1; Activate-Anaconda"
->>>>>>> origin/ed4pimodes
